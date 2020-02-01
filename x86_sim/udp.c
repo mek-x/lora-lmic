625a2ac86a037d3e2cd9667a61052fbf2a5064b6
Kamil Wcislo <mek.xgt@gmail.com>
2020-02-01 23:59:04 +0100

    x86_sim: add udp module

---
+++ x86_sim/udp.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "udp.h"
+
+#define PORT 19535 // 0x4c4f - 'LO'
+
+void transmit(uint8_t *buf, uint8_t bufSize) {
+    int sockfd;
+    struct sockaddr_in     servaddr;
+
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+        // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    int n, len;
+
+    sendto(sockfd, buf, bufSize, 0,
+        (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    close(sockfd);
+}
---
+++ x86_sim/udp.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void transmit(uint8_t *buf, uint8_t bufSize);
