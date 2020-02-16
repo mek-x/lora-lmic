aad23182a0bec53549a155cfcb11a177722653d4
Kamil Wcislo <mek.xgt@gmail.com>
2020-02-16 16:17:32 +0100

    x86_sim/udp: add receive

--- x86_sim/udp.c
+++ x86_sim/udp.c
@@ -11,19 +11,18 @@
 
 #define PORT 19535 // 0x4c4f - 'LO'
 
-void transmit(uint8_t *buf, uint8_t bufSize) {
+void transmit(uint8_t *buf, size_t bufSize) {
     int sockfd;
     struct sockaddr_in     servaddr;
 
     // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0 ) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    int broadcast = 1;
-
-    if(setsockopt(sockfd,SOL_SOCKET,SO_BROADCAST,&broadcast,sizeof(broadcast)) < 0) {
+    int one = 1;
+    if(setsockopt(sockfd,SOL_SOCKET,SO_BROADCAST,&one,sizeof(one)) < 0) {
         perror("Error in setting Broadcast option");
         close(sockfd);
         exit(EXIT_FAILURE);
@@ -34,12 +33,61 @@ void transmit(uint8_t *buf, uint8_t bufSize) {
         // Filling server information
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_LOOPBACK || 0xff;
+    servaddr.sin_addr.s_addr = (in_addr_t) 0xff00007f;
 
-    int n, len;
+    ssize_t n;
 
-    sendto(sockfd, buf, bufSize, 0,
+    n = sendto(sockfd, buf, bufSize, 0,
         (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    if (n < 0)
+        perror("sendto() error:");
+
+    printf("sendto(): sent %d bytes\n", n);
+
+    close(sockfd);
+}
+
+void receive(uint8_t *buf, size_t bufSize) {
+    int sockfd;
+    struct sockaddr_in s_in_me, s_in_other;
+
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int one = 1;
+    if(setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&one,sizeof(one)) < 0) {
+        perror("Error in setting reuse option");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&s_in_me, 0, sizeof(s_in_me));
+
+        // Filling server information
+    s_in_me.sin_family = AF_INET;
+    s_in_me.sin_port = htons(PORT);
+    s_in_me.sin_addr.s_addr = (in_addr_t) 0xff00007f;
+
+    if(bind(sockfd, (const struct sockaddr *)&s_in_me, sizeof(s_in_me)) < 0) {
+        perror("bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t n;
+    socklen_t socklen = sizeof(struct sockaddr_in);
+
+    n = recvfrom(sockfd, buf, bufSize, 0,
+        (struct sockaddr *)&s_in_other, &socklen);
+
+    if (n < 0)
+        perror("recvfrom() error:");
+
+    printf("recvfrom(): received %d bytes\n", n);
 
     close(sockfd);
 }
--- x86_sim/udp.h
+++ x86_sim/udp.h
@@ -1,3 +1,4 @@
 #pragma once
 
-void transmit(uint8_t *buf, uint8_t bufSize);
+void transmit(uint8_t *buf, size_t bufSize);
+void receive(uint8_t *buf, size_t bufSize);
